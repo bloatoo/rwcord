f1c5af80925e4126950cc859768d34df08573c54
bloatoo <bloatoo@tuta.io>
2022-01-16 16:13:32 +0200

    fix example

--- examples/connect.rs
+++ examples/connect.rs
@@ -1,8 +1,12 @@
-use rwcord::Client;
+use rwcord::{Client, Handler};
+
+struct EventHandler {}
+
+impl Handler for EventHandler {}
 
 #[tokio::main]
 async fn main() {
     let token = std::env::var("TOKEN").unwrap();
     let client = Client::new(token);
-    client.start().await.unwrap();
+    client.start::<EventHandler>().await.unwrap();
 }
