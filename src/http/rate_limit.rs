f326395a244010dc4e80e7e93a98c5104be6187d
bloatoo <bloatoo@tuta.io>
2022-01-18 19:14:55 +0200

    Bucket struct

--- src/http/rate_limit.rs
+++ src/http/rate_limit.rs
@@ -2,6 +2,12 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+pub struct Bucket {
+    pub limit: u32,
+    pub remaining: u32,
+    pub reset: u32,
+}
+
 pub struct RateLimits {
     buckets: Arc<Mutex<HashMap<String, Bucket>>>,
 }
